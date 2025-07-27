fa6f49d70719ea46ea5220d5d6951f63e86bf114
anindita-pal-98 <panindita98@gmail.com>
2025-07-28 00:45:28 +0530

    practice code for string manipulation

--- 27July2025/trial.js
+++ 27July2025/trial.js
@@ -1,1 +1,44 @@
-console.log('My first trial script in github actions!');
+// let str = "aaabbcccdeffghij";
+// let result = "";
+// let obj = {};
+// for (let i = 0; i < str.length; i++) {
+//     if (obj.hasOwnProperty(str[i])) {
+//         obj[str[i]] = obj[str[i]] + 1;  
+//     }
+//     else {
+//         obj[str[i]] = 1;
+//     }
+// }
+
+// for (let i in obj) {
+//     if(obj[i] > 1) {
+//         result = result + obj[i] + i;
+//     }
+//     else {
+//         result = result + i;
+//     }
+// }
+// console.log(result);
+
+
+
+let str = "aaabbcccdeffghij";
+
+let count = 1;
+let result = "";
+
+
+for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+        count += 1;
+    } else {
+        if (count > 1) {
+            result += count + str[i];
+        } else {
+            result += str[i];
+        }
+        count = 1;
+    }
+}
+
+console.log("result", result);
